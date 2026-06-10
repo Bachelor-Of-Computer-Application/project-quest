1d24c8d9b6816e77dcf7bea61b7aea74175cf9a1
Manisha Timilsina <timilsinamanisha177@gmail.com>
2026-06-10 20:42:37 +0545

    new

--- Code of bus reservation system.c
+++ Code of bus reservation system.c
@@ -49,4 +49,41 @@ int loginUser(struct Userusers[10],int numUsers,char username[],char password[])
 	}
 }
 // Function to book tickets
-
+void book Ticket(sztruct bus busses[],int numBuses)
+{
+     printf("\nEnter bus number:");
+     int bus number;
+     scanf("%d",&bus number:");
+     
+     
+     
+     // Find the bus with the given bus number
+     int bus index=-1
+     for(int i=0;i<num buses;i++)
+     {
+             if (buses[i].bus number == bus number)
+             {
+                              bus index=i;
+                              break;
+                              }
+                              }
+if(bus Index==-1)
+{
+printf("Bus with Bus number %d not found.\n",bus number);
+}
+else
+{
+    printf("Enter number of seats:");
+    int seats to book;
+    scanf("%d",&seats to book);
+    if(buses[bus index].avilable seats<seats to book)
+    {
+    printf("Sorry,only %d seats are avilable.\n",buses[bus index].avilable seats);
+}
+else
+{
+    buses[bus index].avilable seats = seats to book;
+    printf("Booking successful!%d seat booked on bus number %d.\n",seats to book,bus number);
+}
+}
+}
